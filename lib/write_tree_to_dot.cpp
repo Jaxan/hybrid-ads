b6033eec4cd7a1bcc1d4b263852abcf10bb78570
Joshua Moerman <lakseru@gmail.com>
2015-04-17 16:08:26 +0200

    Adds some docs and a tool to create the dot files for splitting trees and ad. dist. seqs.

--- lib/write_tree_to_dot.cpp
+++ lib/write_tree_to_dot.cpp
@@ -1,49 +1,64 @@
-#include "write_tree_to_dot.hpp"
 #include "adaptive_distinguishing_sequence.hpp"
+#include "read_mealy_from_dot.hpp"
 #include "splitting_tree.hpp"
+#include "write_tree_to_dot.hpp"
 
 #include <fstream>
+#include <iostream>
 
 using namespace std;
 
-template <typename T>
-ostream & operator<<(ostream& out, vector<T> const & x){
-	if(x.empty()) return out;
+template <typename T, typename Fun>
+void print_vec(ostream & out, const vector<T> & x, const string & d, Fun && f) {
+	if (x.empty()) return;
 
 	auto it = begin(x);
-	out << *it++;
-	while(it != end(x)) out << " " << *it++;
-	return out;
+	out << f(*it++);
+	while (it != end(x)) out << d << f(*it++);
 }
 
+static const auto id = [](auto x) { return x; };
 
-void write_splitting_tree_to_dot(const splitting_tree& root, ostream& out_){
-	write_tree_to_dot(root, [](const splitting_tree & node, ostream& out){
-		out << node.states;
-		if(!node.seperator.empty()){
-			out << "\\n" << node.seperator;
+
+void write_splitting_tree_to_dot(const splitting_tree & root, ostream & out_) {
+	write_tree_to_dot(root, [](const splitting_tree & node, ostream & out) {
+		print_vec(out, node.states, " ", id);
+		if (!node.seperator.empty()) {
+			out << "\\n";
+			print_vec(out, node.seperator, " ", id);
 		}
 	}, out_);
 }
 
-void write_splitting_tree_to_dot(const splitting_tree& root, const string& filename){
+void write_splitting_tree_to_dot(const splitting_tree & root, const string & filename) {
 	ofstream file(filename);
 	write_splitting_tree_to_dot(root, file);
 }
 
-void write_adaptive_distinguishing_sequence_to_dot(const adaptive_distinguishing_sequence & root, ostream & out_){
-	write_tree_to_dot(root, [](const adaptive_distinguishing_sequence & node, ostream& out){
-		if(!node.word.empty()){
-			out << node.word;
+
+void write_adaptive_distinguishing_sequence_to_dot(const adaptive_distinguishing_sequence & root, const translation & t, ostream & out_) {
+	const auto symbols = create_reverse_map(t.input_indices);
+	size_t overflows = 0;
+	write_tree_to_dot(root, [&symbols, &overflows](const adaptive_distinguishing_sequence & node, ostream & out) {
+		if (!node.word.empty()) {
+			print_vec(out, node.word, " ", [&symbols](auto x){ return "I" + symbols[x]; });
 		} else {
 			vector<state> I(node.CI.size());
-			transform(begin(node.CI), end(node.CI), begin(I), [](const pair<state, state> p){ return p.second; });
-			out << "I = " << I;
+			transform(begin(node.CI), end(node.CI), begin(I), [](auto p){ return p.second; });
+			if (I.size() < 7) {
+				out << '{';
+				print_vec(out, I, ", ", id);
+				out << '}';
+			} else {
+				overflows++;
+				out << I.size() << " states\\n{" << I[0] << ", ...}";
+			}
 		}
 	}, out_);
+	clog << overflows << " overflows" << endl;
 }
 
-void write_adaptive_distinguishing_sequence_to_dot(const adaptive_distinguishing_sequence & root, string const & filename){
+void write_adaptive_distinguishing_sequence_to_dot(const adaptive_distinguishing_sequence & root, const translation & t, const string & filename) {
 	ofstream file(filename);
-	write_adaptive_distinguishing_sequence_to_dot(root, file);
+	write_adaptive_distinguishing_sequence_to_dot(root, t, file);
 }
--- lib/write_tree_to_dot.hpp
+++ lib/write_tree_to_dot.hpp
@@ -7,15 +7,15 @@
 
 // Generic printer for tree
 template <typename T, typename NodeString>
-void write_tree_to_dot(const T & tree, NodeString && node_string, std::ostream & out){
+void write_tree_to_dot(const T & tree, NodeString && node_string, std::ostream & out) {
 	using namespace std;
-	out << "digraph g {\n";
+	out << "digraph {\n";
 
 	// breadth first
 	int global_id = 0;
 	queue<pair<int, reference_wrapper<const T>>> work;
 	work.push({global_id++, tree});
-	while(!work.empty()){
+	while (!work.empty()) {
 		const auto id = work.front().first;
 		const T & node = work.front().second;
 		work.pop();
@@ -24,9 +24,10 @@ void write_tree_to_dot(const T & tree, NodeString && node_string, std::ostream &
 		node_string(node, out);
 		out << "\"];\n";
 
-		for(auto && c : node.children){
+		for (auto && c : node.children) {
 			int new_id = global_id++;
-			out << "\ts" << id << " -> " << "s" << new_id << ";\n";
+			out << "\ts" << id << " -> "
+			    << "s" << new_id << ";\n";
 			work.push({new_id, c});
 		}
 	}
@@ -38,8 +39,9 @@ void write_tree_to_dot(const T & tree, NodeString && node_string, std::ostream &
 // Specialized printing for splitting trees and dist seqs
 struct splitting_tree;
 void write_splitting_tree_to_dot(const splitting_tree & root, std::ostream & out);
-void write_splitting_tree_to_dot(const splitting_tree & root, std::string const & filename);
+void write_splitting_tree_to_dot(const splitting_tree & root, const std::string & filename);
 
 struct adaptive_distinguishing_sequence;
-void write_adaptive_distinguishing_sequence_to_dot(const adaptive_distinguishing_sequence & root, std::ostream & out);
-void write_adaptive_distinguishing_sequence_to_dot(const adaptive_distinguishing_sequence & root, std::string const & filename);
+struct translation;
+void write_adaptive_distinguishing_sequence_to_dot(const adaptive_distinguishing_sequence & root, const translation & t, std::ostream & out);
+void write_adaptive_distinguishing_sequence_to_dot(const adaptive_distinguishing_sequence & root, const translation & t, const std::string & filename);
